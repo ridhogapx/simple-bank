74fcb3dd1b5519269e40ca20827dc33f96ecb6cc
RageNeko26 <rneko2006@gmail.com>
2023-07-06 00:28:22 +0700

    fix: loop should be bigger than 'n'

--- util/random.go
+++ util/random.go
@@ -20,7 +20,7 @@ func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i := 0; i < k; i++ {
+	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
