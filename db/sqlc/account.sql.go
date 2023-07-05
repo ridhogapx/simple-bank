2a1cffe40eac18cd07c6f013a52d3373433b298d
RageNeko26 <rneko2006@gmail.com>
2023-07-05 22:45:47 +0700

    feat: add delete operation

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -38,6 +38,15 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 	return i, err
 }
 
+const deleteAccount = `-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
+}
+
 const getAccount = `-- name: GetAccount :one
 SELECT id, owner, balance, currency, created_at FROM accounts
 WHERE id = $1 LIMIT 1
