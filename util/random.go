789db42209faff6190571f99a7ff229d9c349c8f
RageNeko26 <rneko2006@gmail.com>
2023-07-06 00:25:59 +0700

    feat: add utils to generate random string

--- util/random.go
+++ util/random.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -13,4 +14,16 @@ func inti() {
 
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < k; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
 }
