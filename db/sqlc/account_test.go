e36d1b7e4a13739883454b7801ea4c0cdcee0800
RageNeko26 <rneko2006@gmail.com>
2023-07-05 23:58:56 +0700

    test: Testing Create Account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,22 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "Bob",
+		Balance:  2000,
+		Currency: "USD",
+	}
 
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	// Test
+	require.Nil(t, err)
+	require.NotEmpty(t, account)
 }
