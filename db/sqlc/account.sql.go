5d5a0f8bbceed56bfe9d04a7a174e883b146f86c
RageNeko26 <rneko2006@gmail.com>
2023-07-05 23:03:34 +0700

    fix: regenerate code

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -68,6 +68,15 @@ func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
 	return err
 }
 
+const deleteEntry = `-- name: DeleteEntry :exec
+DELETE FROM entries WHERE id = $1
+`
+
+func (q *Queries) DeleteEntry(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteEntry, id)
+	return err
+}
+
 const getAccount = `-- name: GetAccount :one
 SELECT id, owner, balance, currency, created_at FROM accounts
 WHERE id = $1 LIMIT 1
