fa35060c6bf2a2fdd5189be8fec394194aa0c6a1
RageNeko26 <rneko2006@gmail.com>
2023-07-06 00:02:10 +0700

    test: add more case

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "Bob",
-		Balance:  2000,
+		Owner:    "Joe",
+		Balance:  5000,
 		Currency: "USD",
 	}
 
@@ -19,4 +19,10 @@ func TestCreateAccount(t *testing.T) {
 	// Test
 	require.Nil(t, err)
 	require.NotEmpty(t, account)
+
+	// Should be same as we inserted before
+	require.Equal(t, arg.Owner, account.Owner, "Owner is not as expected")
+	require.Equal(t, arg.Balance, account.Balance, "Balance is not as expected")
+	require.Equal(t, arg.Currency, account.Currency, "Currency is not as expected")
+
 }
