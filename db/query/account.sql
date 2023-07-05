baf21bbbc554b72593c2c2d0577f7b377c6b855d
RageNeko26 <rneko2006@gmail.com>
2023-07-05 22:30:58 +0700

    feat: adding Read operation

--- db/query/account.sql
+++ db/query/account.sql
@@ -6,3 +6,13 @@ INSERT INTO accounts (
 ) VALUES (
   $1, $2, $3
 ) RETURNING *;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
