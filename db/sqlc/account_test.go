6a5c84b65cd52a99fd40e3605d85e9e45f6d1db8
RageNeko26 <rneko2006@gmail.com>
2023-07-06 00:42:06 +0700

    fix: add random data in test case

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"simple-bank/util"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,9 +10,9 @@ import (
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "Joe",
-		Balance:  5000,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomBalance(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
