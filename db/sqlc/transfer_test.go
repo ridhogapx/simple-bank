f9c9cc763abd74e0e5dd384a0839f1140bad91d8
RageNeko26 <rneko2006@gmail.com>
2023-07-06 16:21:00 +0700

    fix: result must be as expected

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -21,9 +21,26 @@ func TestCreateTransfer(t *testing.T) {
 }
 
 func TestListTransfer(t *testing.T) {
+	expected := []Transfer{
+		{
+			ID:            1,
+			FromAccountID: 3,
+			ToAccountID:   5,
+			Amount:        3000,
+		},
+		{
+			ID:            2,
+			FromAccountID: 3,
+			ToAccountID:   5,
+			Amount:        3000,
+		},
+	}
 	transfer, err := testQueries.ListTransfer(context.Background(), 3)
 
 	// Case: Make sure there's no error and data is not empty
 	require.Nil(t, err)
 	require.NotEmpty(t, transfer)
+
+	// Case: Should be same as we expected
+	require.Equal(t, expected, transfer)
 }
