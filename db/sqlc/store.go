57c373e1af7cc6245282efbcd90da0827d1bcdae
RageNeko26 <rneko2006@gmail.com>
2023-07-06 22:38:06 +0700

    fix: add Transfer result object

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -45,6 +45,11 @@ type TransferTxParams struct {
 }
 
 type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
 // Perform transfer database
