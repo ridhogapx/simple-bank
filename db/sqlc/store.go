08113f09c7dfb9bd48acced880261f600adbb3f8
RageNeko26 <rneko2006@gmail.com>
2023-07-06 13:35:29 +0700

    feat: add params for Transfer Database

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -37,3 +37,17 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 
 	return tx.Commit()
 }
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+}
+
+// Perform transfer database
+func TransferTx() {
+
+}
