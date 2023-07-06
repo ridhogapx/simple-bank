57dad0b0b2a2f1e396db94faa9d709d5a66028b2
RageNeko26 <rneko2006@gmail.com>
2023-07-06 14:33:19 +0700

    fix: add case for listing all entries

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestCreateEntry(t *testing.T) {
 	arg := CreateEntryParams{
-		AccountID: 3,
-		Amount:    6000,
+		AccountID: 5,
+		Amount:    7000,
 	}
 
 	entry, err := testQueries.CreateEntry(context.Background(), arg)
@@ -22,5 +22,37 @@ func TestCreateEntry(t *testing.T) {
 	// Should be equal as we input it
 	require.Equal(t, arg.AccountID, entry.AccountID)
 	require.Equal(t, arg.Amount, entry.Amount)
+
+}
+
+func TestListEntry(t *testing.T) {
+	expected := []Entry{
+		{
+			ID:        1,
+			AccountID: 5,
+			Amount:    2300,
+		},
+		{
+			ID:        2,
+			AccountID: 3,
+			Amount:    6000,
+		}, {
+			ID:        3,
+			AccountID: 5,
+			Amount:    7000,
+		}, {
+			ID:        4,
+			AccountID: 5,
+			Amount:    7000,
+		},
+	}
+	list, err := testQueries.ListEntries(context.Background(), 5)
+
+	// Case: Should not error and data is not empty!
+	require.Nil(t, err)
+	require.NotEmpty(t, list)
+
+	// Case: Should as we expected
+	require.Equal(t, expected, list)
 
 }
