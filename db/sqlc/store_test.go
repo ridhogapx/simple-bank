828dbe903f03f707857d11a2a4887bc4fcea4c0d
RageNeko26 <rneko2006@gmail.com>
2023-07-07 01:00:19 +0700

    fix: should use goroutine to handle transaction

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"testing"
 )
 
@@ -11,5 +12,32 @@ func TestTransaction(t *testing.T) {
 	// Generate random account
 	account_a := generateRandomAccount(t)
 	account_b := generateRandomAccount(t)
+
+	// Balanace that we're gonna transfer
+	amount := int64(500)
+
+	// Should be run in n concurrent
+	n := 5
+
+	// Channel for handling communication data
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account_a.ID,
+				ToAccountID:   account_b.ID,
+				Amount:        amount,
+			})
+
+			if err != nil {
+				errs <- err
+			}
+
+			results <- result
+
+		}()
+	}
 
 }
